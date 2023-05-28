3111d16821b9ddc3d043ad147d4b279bd001714f
Dmitry Vishniakov <112121dfd@gmail.com>
2023-05-28 18:59:46 +0700

    Change Radio.java and add tests

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -2,68 +2,83 @@ package ru.netology;
 
 public class Radio {
     private int radioStation;
+    private int maxRadioStation = 9; // мах радиостанция
+    private int minRadioStation = 0; // мин радиостанция
+    private int sizeRadioStation = 10; // колличество радиостанций
     private int volume;
+    private int minVolume = 0; // мин громкость
+    private int maxVolume = 100; // макс громкость
+    public Radio(int sizeRadioStation) {
+        this.sizeRadioStation = sizeRadioStation;
+        this.maxRadioStation = sizeRadioStation - 1;
+    }
+    public Radio (){
+    }
+
 
     public int getRadioStation() {
         return radioStation;
     }
+    public int getMaxRadioStation() {
+        return maxRadioStation;
+    }
+
+    public int getMinRadioStation() {
+        return minRadioStation;
+    }
 
     public void setRadioStation(int newRadioStation) {
-        if (newRadioStation < 0) {
+        if (newRadioStation < minRadioStation) {
             return;
         }
-        if (newRadioStation > 9) {
+        if (newRadioStation > maxRadioStation) {
             return;
         }
         radioStation = newRadioStation;
     }
-
     public int getVolume() {
         return volume;
     }
 
     public void setVolume(int newVolume) {
-        if (newVolume < 0) {
+        if (newVolume < minVolume) {
             return;
         }
-        if (newVolume > 100) {
+        if (newVolume > maxVolume) {
             return;
         }
         volume = newVolume;
     }
 
     public void next() { // метод переключения следующей станции
-        if (radioStation < 9) {
+        if (radioStation < maxRadioStation) {
             radioStation++;
         } else {
-            radioStation = 0;
+            radioStation = minRadioStation;
         }
     }
 
     public void prev() { // метод переключения на предыдущую станцию
-        if (radioStation > 0) {
+        if (radioStation > minRadioStation) {
             radioStation--;
         } else {
-            radioStation = 9;
+            radioStation = maxRadioStation;
         }
 
     }
-
     public void increaseVolume() { // метод увеличения громкости на 1
-        if (volume < 100) {
+        if (volume < maxVolume) {
             volume++;
         } else {
-            volume = 100;
+            volume = maxVolume;
         }
     }
 
     public void reduceVolume() { // метод уменьшения громкости на 1
-        if (volume > 0) {
+        if (volume > minVolume) {
             volume--;
         } else {
-            volume = 0;
+            volume = minVolume;
         }
     }
-
-
 }
